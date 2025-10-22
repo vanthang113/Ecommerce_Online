405687a3629ec44e6527bd85af044b62e240eba7
vanthang123 <vanthang2222147@gmail.com>
2025-10-22 20:02:54 +0700

    thêm phần github/workflows/ để deploy

--- front-end/src/components/Footer.tsx
+++ front-end/src/components/Footer.tsx
@@ -1,19 +1,3 @@
-// export default function Footer() {
-//   return (
-//     <footer className="bg-gray-800 text-white py-6 mt-10">
-//       <div className="container mx-auto text-center">
-//         <p className="text-sm">© 2025 MyShop. All rights reserved.</p>
-//         <p className="text-sm mt-2">
-//           Liên hệ:{" "}
-//           <a href="mailto:support@myshop.com" className="underline">
-//             vanthang2222147@gmail.com
-//           </a>
-//         </p>
-//       </div>
-//     </footer>
-//   );
-// }
-///////////////////
 export default function Footer() {
   return (
     <footer className="bg-gray-100 text-gray-700 text-sm mt-10 border-t">
